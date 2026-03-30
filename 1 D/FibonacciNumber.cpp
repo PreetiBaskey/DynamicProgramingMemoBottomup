5a5eebc0d51048a59bda09f72d7559c0760b0ed0
React Native Developer <38905783+PreetiBaskey@users.noreply.github.com>
2026-03-30 08:56:14 +0530

    Create FibonacciNumber.cpp

---
+++ 1 D/FibonacciNumber.cpp
@@ -0,0 +1,47 @@
+//approach - 1 - using recursion
+//time complexity - O(2^n) 
+//space complexity - O(1)
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n == 0) {
+//             return 0;
+//         }
+//         if(n == 1) {
+//             return 1;
+//         }
+
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// };
+
+
+
+
+
+//approach - 2 - using dp
+//time complexity - O(n)
+//space complexity - O(n)
+class Solution {
+public:
+    int solveWithDP(vector<int> &dp, int n) {
+        if(n <= 1) {
+            return n;
+        }
+
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+
+        return dp[n] = solveWithDP(dp, n - 1) + solveWithDP(dp, n - 2);
+    }
+    int fib(int n) {
+        if(n <= 1){
+            return n;
+        }
+
+        vector<int> dp(n + 1, -1);
+
+        return solveWithDP(dp, n);
+    }
+};
